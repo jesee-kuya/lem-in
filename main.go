b3f104ef88be9e4bddf268163c7372f71806eebe
jamos <amosjoel91@gmail.com>
2024-12-10 15:26:29 +0300

    feat: read file contents

--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"lem-in/read"
 )
 
 func main() {
 	args := os.Args[1:]
-	if len(args) < 2 {
+	if len(args) != 1 {
 		fmt.Println("Usage: go run main.go [input_file]")
 		os.Exit(1)
 	}
+	// process the file contents
+	file, err := read.ReadFile(args[0])
+	if err != nil {
+		fmt.Println("ERROR", err)
+	}
+	fmt.Println(file)
 }
