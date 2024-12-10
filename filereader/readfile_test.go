fe6a5d8991b1b0a5b31addc5e56578a99c432311
jkuya <kuyajesee@proton.me>
2024-12-10 15:14:59 +0300

    Test: add a test for reading the contents of a file

---
+++ filereader/readfile_test.go
@@ -0,0 +1,29 @@
+package filereader
+
+import (
+	"os"
+	"testing"
+)
+
+var TestCases = []struct {
+	name     string
+	filepath string
+	content  []byte
+	output   string
+}{
+	{"Test1", "test1.txt", []byte("Hello World"), "Hello World"},
+}
+
+func TestReadFile(t *testing.T) {
+	for _, tc := range TestCases {
+		err := os.WriteFile(tc.filepath, tc.content, 0o666)
+		if err != nil {
+			t.Errorf("Error creating file %v", tc.filepath)
+		}
+		content, _ := ReadFile(tc.filepath)
+
+		if content != tc.output {
+			t.Errorf("Expected\ncontent: %v but got\ncontent %v", tc.output, content)
+		}
+	}
+}
