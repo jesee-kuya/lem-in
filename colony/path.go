4ffe1628a6c5880685c010a908c7a3aa28acd794
John <johneliud4@gmail.com>
2025-01-16 18:40:32 +0300

    add: calculateOptimalDistribution function that calculates the time taken for an ant to reach the end room in order to determine the optimal path.

--- colony/path.go
+++ colony/path.go
@@ -6,8 +6,9 @@ import (
 )
 
 type PathInfo struct {
-	path             []int
-	length, antCount int
+	path     []int
+	length   int
+	antCount int
 }
 
 func ParseAnts(lines []string) int {
@@ -24,6 +25,49 @@ func ParseAnts(lines []string) int {
 	return ants
 }
 
+// calculateOptimalDistribution determines how many ants should take each path
+func calculateOptimalDistribution(paths [][]int, numberOfAnts int) []PathInfo {
+	if len(paths) == 0 || numberOfAnts <= 0 {
+		return nil
+	}
+
+	pathInfos := make([]PathInfo, len(paths))
+	for i, path := range paths {
+		pathInfos[i] = PathInfo{
+			path:     path,
+			length:   len(path) - 1,
+			antCount: 0,
+		}
+	}
+
+	// Sort paths by length
+	for i := 0; i < len(pathInfos)-1; i++ {
+		for j := i + 1; j < len(pathInfos); j++ {
+			if pathInfos[i].length > pathInfos[j].length {
+				pathInfos[i], pathInfos[j] = pathInfos[j], pathInfos[i]
+			}
+		}
+	}
+
+	// Distribute ants optimally
+	remainingAnts := numberOfAnts
+	for remainingAnts > 0 {
+		bestPathIndex := 0
+		bestCompletionTime := (pathInfos[0].antCount + 1 + pathInfos[0].length)
+
+		for i := 1; i < len(pathInfos); i++ {
+			completionTime := (pathInfos[i].antCount + 1 + pathInfos[i].length)
+			if completionTime < bestCompletionTime {
+				bestCompletionTime = completionTime
+				bestPathIndex = i
+			}
+		}
+		pathInfos[bestPathIndex].antCount++
+		remainingAnts--
+	}
+	return pathInfos
+}
+
 func Path(routes [][]int, numberOfAnts int) {
 	if len(routes) == 0 || numberOfAnts <= 0 {
 		return
@@ -32,15 +76,15 @@ func Path(routes [][]int, numberOfAnts int) {
 	pathInfos := calculateOptimalDistribution(routes, numberOfAnts)
 
 	type AntState struct {
-		pathIndex, position int
-		isFinished          bool
+		pathIndex  int
+		position   int
+		isFinished bool
 	}
 
-	antStates := make([]AntState, numberOfAnts)
+	antStates := make([]AntState, numberOfAnts+1)
 	currentAnt := 1
-	var antsFinished int
+	antsFinished := 0
 
-	// Assign initial paths to ants
 	for i := range pathInfos {
 		for j := 0; j < pathInfos[i].antCount; j++ {
 			if currentAnt <= numberOfAnts {
@@ -67,10 +111,9 @@ func Path(routes [][]int, numberOfAnts int) {
 			state := &antStates[ant]
 			path := pathInfos[state.pathIndex].path
 
-			// Check if ant can start if it hasn't started yet
+			// If ant hasn't started yet, check if it can start
 			if state.position == -1 {
 				nextRoom := path[1]
-
 				if !occupiedRooms[nextRoom] {
 					state.position = 1
 					occupiedRooms[nextRoom] = true
@@ -79,10 +122,9 @@ func Path(routes [][]int, numberOfAnts int) {
 				continue
 			}
 
-			// Try moving forward if ant is on the path
+			// If ant is on the path, try to move forward
 			if state.position < len(path)-1 {
 				nextRoom := path[state.position+1]
-
 				if !occupiedRooms[nextRoom] {
 					state.position++
 					occupiedRooms[nextRoom] = true
@@ -101,22 +143,4 @@ func Path(routes [][]int, numberOfAnts int) {
 			fmt.Println(strings.Join(moves, " "))
 		}
 	}
-}
-
-func assignRoutes(numberOfAnts int, routes [][]int) map[int][]int {
-	assignedRoutes := make(map[int][]int)
-
-	for i := 1; i <= numberOfAnts; i++ {
-		assignedRoutes[i] = routes[(i-1)%len(routes)]
-	}
-	return assignedRoutes
-}
-
-func isRoomOccupied(antPositions []int, room int) bool {
-	for _, position := range antPositions {
-		if position == room {
-			return true
-		}
-	}
-	return false
 }
