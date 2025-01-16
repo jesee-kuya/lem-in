bfab81c7f7d54fbee5582b8258f6cfe745cb386d
John <johneliud4@gmail.com>
2025-01-16 17:18:39 +0300

    add: ParseAnts function to check whether ants can be parsed from a read input.

--- colony/path.go
+++ colony/path.go
@@ -1,5 +1,21 @@
 package colony
 
+import "fmt"
+
+func ParseAnts(lines []string) int {
+	if len(lines) == 0 {
+		return 0
+	}
+
+	var ants int
+	_, err := fmt.Sscanf(lines[0], "%d", &ants)
+	if err != nil {
+		fmt.Println("Error parsing number of ants:", err)
+		return 0
+	}
+	return ants
+}
+
 func isRoomOccupied(antPositions []int, room int) bool {
 	for _, position := range antPositions {
 		if position == room {
