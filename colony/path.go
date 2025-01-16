074bea92a477865bff4b6435ad53145bb72682ff
John <johneliud4@gmail.com>
2025-01-16 17:19:53 +0300

    add: Path function to display the distribution of ants based on the provided input.

--- colony/path.go
+++ colony/path.go
@@ -1,6 +1,9 @@
 package colony
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func ParseAnts(lines []string) int {
 	if len(lines) == 0 {
@@ -14,6 +17,45 @@ func ParseAnts(lines []string) int {
 		return 0
 	}
 	return ants
+}
+
+func Path(routes [][]int, numberOfAnts int) {
+	antPositions := make([]int, numberOfAnts+1)
+	antsFinished := 0
+
+	// Iterate turn by turn
+	for i := 0; antsFinished < numberOfAnts; i++ {
+		var moves []string
+
+		for ant := 1; ant <= numberOfAnts; ant++ {
+			if antPositions[ant] == -1 {
+				continue // Ant is already at ##end
+			}
+
+			for _, route := range routes {
+				currentPosition := antPositions[ant]
+
+				if currentPosition < len(route)-1 { // Ant has not finished the path
+					nextRoom := route[currentPosition+1]
+
+					if !isRoomOccupied(antPositions, nextRoom) || nextRoom == route[len(route)-1] {
+						antPositions[ant]++
+						moves = append(moves, fmt.Sprintf("L%d-%d", ant, nextRoom))
+
+						if nextRoom == route[len(route)-1] {
+							antPositions[ant] = -1 // Marks ant as finished
+							antsFinished++
+						}
+						break
+					}
+				}
+			}
+		}
+
+		if len(moves) > 0 {
+			fmt.Println(strings.Join(moves, " "))
+		}
+	}
 }
 
 func isRoomOccupied(antPositions []int, room int) bool {
